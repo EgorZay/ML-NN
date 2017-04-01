ed37ee0302f77900d7e0b96a90142aa9ac4f0607
151337 <eleven@gscw.ru>
2017-04-01 20:02:47 +0800

    upd ex

--- loss_functions/sigmoid_loss.py
+++ loss_functions/sigmoid_loss.py
@@ -2,9 +2,10 @@ import numpy as np
 import tensorflow as tf
 
 x = tf.placeholder(tf.float32, [10.])  # may pass n-dimension list
-beta = tf.placeholder([0])  # tf.float32 is implicitly defined; keep dimensions relevant
-alpha = tf.placeholder([0])
+beta = tf.Variable([1.])  # tf.float32 is implicitly defined; keep dimensions relevant
+alpha = tf.Variable([5.])
 
+y_true = tf.placeholder()
 y = tf.matmul(x, beta) + alpha  # basic linear model
 # matmul is matrix multiplication
 
@@ -12,9 +13,16 @@ cross_entropy = tf.reduce_mean(
   tf.nn.sigmoid_cross_entropy_with_logits(labels=y_true, logits=y)
 )  # labels equals to target, y serves as preds
 
-#
-#
-# Manual sigmoid function and cross-entropy loss
+# session run
+ses = tf.Session()
+init = tf.global_variables_initializer()
+ses.run(init)
+print(sess.run(y, {x: [1, 2, 3, 4]}))
+
+print(sess.run(cross_entropy, {x: [1, 2, 3, 4], y_true: [-5.5, -6.5, 7.5, 8.5]}))
+
+
+# manual sigmoid function and cross-entropy loss
 
 def sigmoidFunction(pred):
   return 1 / (np.exp(-pred))
